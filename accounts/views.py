7f5962135cfc387a4fb16c69f8eb6c24e77e914a
Abhishikth Aleti <abhishikth1997@gmail.com>
2018-03-05 05:04:48 +0530

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'accounts/login.html')
+    numbers = [1,2,3,4]
+    name = "Abhishikth"
+    args = {'Name':name,'numbers':numbers}
+    return render(request,'accounts/home.html',args)
